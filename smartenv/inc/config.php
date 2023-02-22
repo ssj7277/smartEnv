ed5a7b855cac7661ea6228b68d199f8397c5b0f6
jenifer <sonsj7277@gmail.com>
2023-02-22 13:46:47 +0900

    230222

---
+++ smartenv/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+	//header
+	//header('Content-Type: text/html; charset=UTF-8');
+
+	//session
+	//session_start();
+
+	require_once('/var/www/html/smartenv/inc/define.php');	
---
+++ smartenv/inc/define.php
@@ -0,0 +1,15 @@
+<?php
+    define('DBHOST',        'localhost');
+    define('DBUSERNAME',    'sjson');
+    define('DBPW',          'sj7277');
+    define('DBNAME',        'smartenv');
+
+	error_reporting(E_ALL);
+
+	ini_set('display_errors', '1');
+    //DB include
+    require_once('/var/www/html/smartenv/inc/dbconnect.php');
+		
+	//paging
+	define('PAGING_SIZE',       10);
+	define('PAGING_SCALE',      10);
