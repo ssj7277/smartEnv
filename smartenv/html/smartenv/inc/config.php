7fc719056da97c4969ec4cdc6957ddc5c894750e
jenifer <sonsj7277@gmail.com>
2023-02-22 14:23:44 +0900

    create

--- smartenv/html/smartenv/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//header
-//header('Content-Type: text/html; charset=UTF-8');
-
-//session
-//session_start();
-
-require_once('/var/www/html/smartenv/inc/define.php');
--- smartenv/html/smartenv/inc/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_mysqli = @new mysqli(DBHOST, DBUSERNAME, DBPW, DBNAME);
-
-if ($_mysqli->connect_errno) {
-    die('Connect Error : ' . $_mysqli->connect_errno);
-}
-
-$_mysqli->set_charset('utf8');
