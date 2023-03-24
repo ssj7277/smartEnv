e90c9dec497c8320bc35eefcf654e65538527436
ssj7277 <125252336+ssj7277@users.noreply.github.com>
2023-03-24 16:03:11 +0900

    Create myScript.js

---
+++ assets/js/myScript.js
@@ -0,0 +1,24 @@
+
+
+function searchEnv() {
+	var month = document.getElementById('month').value;
+	var house = document.getElementById('sensor').value;
+
+	if (house == '0') {
+		alert('하우스를 선택하세요!!');
+		return false;
+	}
+	if (month == '0'){
+		alert('기간을 선택하세요!!');
+		return false;
+	}
+	envSearchForm.submit();
+	
+	console.log(month);
+}
+
+function selectAll() {
+	window.location.href = "http://192.168.50.5/html/smartenv/index.php";
+}
+
+
